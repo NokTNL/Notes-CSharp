dbc055b951ad07408807a3e0ceaf3ddecaf0a5a2
NokTNL <noklamtsz@gmail.com>
2025-01-21 17:22:26 +0000

    Add JsonPlaceholder Exercise

---
+++ Exerceise-2-JsonPlaceholder/Post.cs
@@ -0,0 +1,8 @@
+namespace Exerceise_2_JsonPlaceholder;
+
+class Post {
+    public required int Id {get; init;}
+    public required string Title {get; init;}
+    public required string Body {get; init;}
+    public required int UserId {get; init;}
+}
---
+++ Exerceise-2-JsonPlaceholder/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Http.Json;
+using Exerceise_2_JsonPlaceholder;
+
+HttpClient httpClient = new()
+{
+    BaseAddress = new Uri("https://jsonplaceholder.typicode.com")
+};
+
+// GetFromJsonAsync deserializes the JSON reponse to the specified type (e.g. class / record)!
+var posts = (await httpClient.GetFromJsonAsync<List<Post>>("/posts"))!; // Methods like this and `GetStringAsync` calls EnsureSuccessStatusCode() internally, so it will throw if status is not successful
+
+Console.WriteLine($"Count: {posts.Count()}");
+
+var decendingPostsTruncated = posts
+    .Where(post => post.Id % 7 == 0)
+    .OrderByDescending(post => post.Id)
+    .Take(10)
+    .Select(post => new {post.Id, post.Title});
+foreach (var post in decendingPostsTruncated)
+{
+    Console.WriteLine($"{post.Id}: {post.Title}");
+}
+
+
